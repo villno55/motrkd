4e2868b46658ffdf2eacc209fc25d45f6ce9bd59
villno55 <adsotrabajos24@gmail.com>
2026-03-18 12:44:51 -0500

    corregido

--- users/api/views.py
+++ users/api/views.py
@@ -1,11 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from users.api import serializers
 from users.models import User
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAdminUser
 from users.api.serializers import UserSerializer
 
 
 class UserApiViewSet(ModelViewSet):
-    permission_classes = [AllowAny]  # define los permisos del CRUD
+    permission_classes = [IsAdminUser]  # define los permisos del CRUD
     serializer_class = UserSerializer  # manipulacion de datos como lo quiero yo
     queryset = User.objects.all()
